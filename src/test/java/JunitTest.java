f563916385b587dc0c6220c0d57844d5dab9854f
Rain238 <3164395730@qq.com>
2023-02-25 02:16:13 +0800

    JunitTest测试保留数据

--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -1,12 +1,13 @@
-import com.light.rain.util.Charset;
-import com.light.rain.util.FileUtil;
-import com.light.rain.util.UniqueIdUtil;
+import com.light.rain.build.Builder;
+import com.light.rain.example.pojo.Student;
+import com.light.rain.util.*;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.light.rain.util.Base64Util.*;
 import static com.light.rain.util.DateFormatUtil.getTime;
@@ -87,6 +88,7 @@ public class JunitTest {
         System.out.println("UniqueIdUtil.getSnowflakesId() = " + UniqueIdUtil.getSnowflakesId());
         System.out.println("UniqueIdUtil.getObjectId() = " + UniqueIdUtil.getObjectId());
     }
+
     /**
      * @Author: LightRain
      * @Date: 23/2/2023 下午 5:35
@@ -99,7 +101,7 @@ public class JunitTest {
         System.out.println("FileUtil = " + FileUtil.readData("D:\\项目\\alicia\\alicia\\src\\main\\resources\\url.txt"));
         List<String> list = FileUtil.readData("D:\\项目\\alicia\\alicia\\src\\main\\resources\\url.txt");
         for (String s : list) {
-            FileUtil.writeData("D:\\项目\\alicia\\alicia\\src\\main\\resources\\123456.txt",s,true);
+            FileUtil.writeData("D:\\项目\\alicia\\alicia\\src\\main\\resources\\123456.txt", s, true);
         }
     }
 
@@ -115,24 +117,184 @@ public class JunitTest {
         List<String> list = new LinkedList<>();
         list.add("https://t4.wodetu.cn/2022/10/19/d297b2a8e02ade7642969f6306121748.png");
         list.add("https://cdn-cookieyes.com/client_data/b625465893342370098580f1/audit-table/YMZEyO_0.json");
-        FileUtil.copyURLToFile(list,"D:\\项目\\alicia\\alicia\\src\\main\\resources\\16.txt");
-        FileUtil.copyURLToFile("https://cdn-cookieyes.com/client_data/b625465893342370098580f1/audit-table/YMZEyO_0.json","D:\\项目\\alicia\\alicia\\src\\main\\resources\\17.txt");
+        FileUtil.copyURLToFile(list, "D:\\项目\\alicia\\alicia\\src\\main\\resources\\16.txt");
+        FileUtil.copyURLToFile("https://cdn-cookieyes.com/client_data/b625465893342370098580f1/audit-table/YMZEyO_0.json", "D:\\项目\\alicia\\alicia\\src\\main\\resources\\17.txt");
     }
 
     @Test
     public void FileUtilTest3() throws IOException {
         List<String> list = FileUtil.readData("D:\\项目\\alicia\\alicia\\src\\main\\resources\\url.txt");
         for (String s : list) {
-            FileUtil.writeData("D:\\008.txt",s, Charset.UTF_8,true);
+            FileUtil.writeData("D:\\008.txt", s, Charset.UTF_8, true);
         }
     }
 
     @Test
     public void FileUtilTest4() throws IOException {
-        System.out.println("FileUtils.byteCountToDisplaySize(1000) = " + FileUtils.byteCountToDisplaySize((long) (1024*1024*1024)));
+        System.out.println("FileUtils.byteCountToDisplaySize(1000) = " + FileUtils.byteCountToDisplaySize((long) (1024 * 1024 * 1024)));
         int urlFileSize = FileUtil.getURLFileSize("https://t4.wodetu.cn/2022/10/19/d297b2a8e02ade7642969f6306121748.png");
         System.out.println(" = >>>>>" + FileUtil.copyURLToFileSize("https://t4.wodetu.cn/2022/10/19/d297b2a8e02ade7642969f6306121748.png", "D:\\项目\\alicia\\alicia\\src\\main\\resources\\", 9865536));
         System.out.println("urlFileSize = " + urlFileSize);
+    }
+
+    @Test
+    public void BuilderTest() {
+        Student 张三 = Builder.builder(Student::new).link(Student::setName, "张三").link(Student::setAge, 16).build();
+        System.out.println("张三 = " + 张三.getName());
+        System.out.println("张三 = " + 张三.getAge());
+    }
+
+    @Test
+    public void BuilderTest1() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("张三", 12));
+        list.add(new Student("李四", 17));
+        Map<String, Student> collect = list.stream().collect(Collectors.toMap(Student::getName, value -> value, (k1, k2) -> k1));
+        collect.values().forEach(a -> System.out.println(a.getName() + a.getAge()));
+    }
+
+    @Test
+    public void MapUtilTest1() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("张三", 16));
+        list.add(new Student("李四", 17));
+        Map<String, Student> map = CollectionUtil.listToMap(list, Student::getName);
+        map.values().forEach(a -> System.out.println(a.getName() + a.getAge()));
+
+    }
+
+    @Test
+    public void MapUtilTest2() {
+        Set<Student> set = new HashSet<>();
+        set.add(new Student("张三", 18));
+        set.add(new Student("张三", 19));
+        set.add(new Student("李四", 17));
+        Map<String, Student> map = CollectionUtil.setToMap(set, Student::getName, val -> val, (k1, k2) -> k2);
+        map.values().forEach(a -> System.out.println(a.getName() + a.getAge()));
+    }
+
+    @Test
+    public void MapUtilTest3() {
+        Set<Student> list = new HashSet<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("李四", 17));
+        Set<Student> collect = CollectionUtil.filter(list, user -> user.getAge() > 16);
+        collect.forEach(a -> System.out.println(a.getName()));
+    }
+
+    @Test
+    public void MapUtilTest4() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("张三", 12));
+        list.add(new Student("李四", 17));
+        Map<String, List<Student>> collect1 = CollectionUtil.groupingBy(list, Student::getName);
+        collect1.forEach((k, v) -> {
+            System.out.println("k = " + k);
+            if (k.contains("张三")) {
+                for (Student student : v) {
+                    System.out.println("student.getName() = " + student.getName());
+                    System.out.println("student.getAge() = " + student.getAge());
+                }
+            }
+        });
+        Set<Student> set = new HashSet<>();
+        set.add(new Student("张三", 16));
+        set.add(new Student("张三", 18));
+        set.add(new Student("李四", 17));
+        set.add(new Student("李四", 17));
+        Map<String, Set<Student>> collect2 = CollectionUtil.groupingBy(set, Student::getName);
+        collect2.forEach((k, v) -> {
+            System.out.println("k = " + k);
+            if (k.contains("张三")) {
+                for (Student student : v) {
+                    System.out.println("student.getName() = " + student.getName());
+                    System.out.println("student.getAge() = " + student.getAge());
+                }
+            }
+        });
+    }
+
+    @Test
+    public void MapUtilTest5() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("张三", 12));
+        list.add(new Student("李四", 17));
+//        List<Student> collect = list.stream().sorted(Comparator.comparing(Student::getAge)).toList();
+        List<Student> sorted = CollectionUtil.sorted(list, Student::getAge);
+        sorted.forEach(i -> System.out.println(i.getName() + i.getAge()));
+    }
+
+    @Test
+    public void MapUtilTest6() {
+        Set<Student> set = new HashSet<>();
+        set.add(new Student("张三", 16));
+        set.add(new Student("王五", 12));
+        set.add(new Student("李四", 17));
+//        List<Student> collect = list.stream().sorted(Comparator.comparing(Student::getAge)).toList();
+        Set<Student> sorted = CollectionUtil.sortedReversed(set, Student::getAge);
+        sorted.forEach(i -> System.out.println(i.getName() + i.getAge()));
+    }
+
+    @Test
+    public void MapUtilTest7() {
+        List<String> list = Arrays.asList("A", "A", "B", "C");
+        List<String> distinct = CollectionUtil.distinct(list);
+        distinct.forEach(System.out::println);
+    }
+
+    @Test
+    public void MapUtilTest8() {
+        List<String> list = Arrays.asList("A", "A", "B", "C");
+        boolean a = CollectionUtil.anyMatch(list, s -> s.contains("A"));
+        System.out.println("a = " + a);
+        Set<String> set = Set.of("A", "B", "C");
+        boolean a2 = CollectionUtil.anyMatch(set, s -> s.contains("A"));
+        boolean b = CollectionUtil.allMatch(list, s -> s.contains("B"));
+        System.out.println("b = " + b);
+    }
+
+    @Test
+    public void MapUtilTest9() {
+        Integer reduce = Stream.of(1, 2, 3, 4, 5).reduce(0, (a, b) -> a + b);
+        System.out.println("reduce = " + reduce);
+        Integer reduce1 = CollectionUtil.reduce(0, Integer::sum, 1, 2, 3, 4, 5);
+        System.out.println("reduce1 = " + reduce1);
+    }
+
+    @Test
+    public void MapUtilTest10() {
+        List<String> collect = Stream.of("程序员田螺", "捡田螺的小男孩", "捡瓶子的小男孩").filter(a -> a.contains("田螺")).peek(a -> System.out.println("关注公众号：" + a)).toList();
+        System.out.println("collect = " + collect);
+        List<String> peek = CollectionUtil.peek(a -> System.out.println("关注公众号：" + a), c -> c.contains("田螺"), "程序员田螺", "捡田螺的小男孩", "捡瓶子的小男孩");
+        System.out.println("peek = " + peek);
+    }
+
+    @Test
+    public void MapUtilTest11() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("李四", 17));
+        Optional<Student> max1 = CollectionUtil.min(list, Student::getAge);
+        max1.ifPresent(info -> System.out.println(info.getName()+info.getAge()));
+        Set<Student> set = new HashSet<>();
+        set.add(new Student("张三", 16));
+        set.add(new Student("李四", 17));
+        Optional<Student> max2 = CollectionUtil.max(set, Student::getAge);
+        max2.ifPresent(info -> System.out.println(info.getName() + info.getAge()));
+    }
+
+    @Test
+    public void MapUtilTest12() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 16));
+        list.add(new Student("张三", 18));
+        list.add(new Student("李四", 17));
+        long count = CollectionUtil.count(list,stu->stu.getAge()>16);
+        System.out.println("count = " + count);
     }
 
 }
